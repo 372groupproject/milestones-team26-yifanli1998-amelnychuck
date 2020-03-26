5613e518ab060b44557f2b34d9e97df02683ea58
yifanli1998 <39859775+yifanli1998@users.noreply.github.com>
2020-03-25 23:42:09 -0700

    Milestone1
    Report 1 and a simple program

---
+++ README.txt.txt
@@ -0,0 +1,3 @@
+3/25/2020
+-File Created: first_program.m
+	behavior: Print out "Hello World", and calculate the area of circle with radius = 2.
---
+++ code/first_program.m
@@ -0,0 +1,8 @@
+disp("Hellow World!")
+
+disp("I like WenKang, since he is a nice guy!")
+
+format long %define the accuracy of the data
+pi = pi 
+r = 2
+s = pi * r^2 %calculate the area of a circle with radius = 2.
